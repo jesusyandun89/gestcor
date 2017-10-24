9879ade6f61013ee60c4cce0e7284c12eb287d78
Jesus Yandun SIS <jyandun@grupotvcable.com>
2017-10-24 10:27:16 -0500

    Agregar archivos de proyecto.

---
+++ GestCor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GestCor.Startup))]
+namespace GestCor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
